2b3cf31df66a377c40af0769406803059cea1568
Filip Minx <filip.minx2@gmail.com>
2020-01-25 20:29:04 +0100

    Cleanup

--- Minx.AOPExample/Minx.AOPExample/Program.cs
+++ Minx.AOPExample/Minx.AOPExample/Program.cs
@@ -1,10 +1,4 @@
-﻿using Castle.DynamicProxy;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System;
 
 namespace Minx.AOPExample
 {
